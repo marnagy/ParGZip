71a8f9b4674e0adc4ceaefa231f9456de8ac56a0
Marek Nagy <62247071@cuni.cz>
2021-06-08 18:51:52 +0200

    Add project files.

---
+++ VeeamTest/SharedParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeeamTest
+{
+	public class SharedParams
+	{
+		public int[] BytesRead { get; set; }
+		public int[] BytesWritten { get; set; }
+		public int[] BytesInMemoryStream { get; set; }
+	}
+}
